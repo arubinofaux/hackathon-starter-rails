d0913128e5d6d3b4ff8346d4d7117a532aa77d68
Dan Vera <dan@yovu.co>
2014-05-02 11:27:20 -0400

    secret should be change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackathonStarterRails::Application.config.secret_key_base = 'c00215b880e88780917c1a40cc45f3d1d433d7163107ce4368d1450d87c15b25f4bf211ed67b4e47e4aba8a2fca1c851b03fc4e31a383796210fa2dada3035f1'
--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 20140210113330) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
